bb1e60712c3694a2652db02cf1a99431bc8a0193
Ricardo Balk <14904229+ricardobalk@users.noreply.github.com>
2023-03-19 03:52:43 +0100

    Extended theme colors.

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,24 @@ module.exports = {
     "./app.vue",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: {
+          25:  "#fafafa",
+          50:  "#f2f2f2",
+          100: "#e6e6e6",
+          200: "#cccccc",
+          300: "#b3b3b3",
+          400: "#999999",
+          500: "#808080",
+          600: "#666666",
+          700: "#4d4d4d",
+          800: "#333333",
+          900: "#1a1a1a",
+          925: "#0d0d0d",
+        },
+      },
+    },
   },
   variants: {
     extend: {},
